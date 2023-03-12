e074da4e9b91169aec5262d4fa6aa2a88f9ed3b6
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2023-03-11 23:17:37 -0800

    added ability to generate summary statistics for topic networks

--- R/network_attr.R
+++ R/network_attr.R
@@ -5,26 +5,51 @@ library(intergraph)
 #igr <- graph_from_adjacency_matrix(adj_mat_subset, mode = "undirected", 
 #                                   weighted = TRUE, diag = FALSE)
 
+#CHOOSE ONE
+type = "governance"
+type = "topic"
+
 gsp_text_with_meta <- readRDS("data/prepped_for_sna")
 gspids <- unique(gsp_text_with_meta$gsp_id)
+rm(gsp_text_with_meta)
 
-network_properties <-  data.frame(matrix(NA, nrow = length(gspids), ncol=7))
+network_properties <-  data.frame(matrix(NA, nrow = length(gspids), ncol=8))
 names(network_properties) <- c("gsp_id", "num_nodes", "num_edges", "density", 
-                               "connectedness", "centralization", "num_communities")
+                               "connectedness", "centralization", 
+                               "num_communities","transitivity")
 
 for(m in 1:length(gspids)){
-  agency_net <- readRDS(paste0("data/network_",gspids[m]))
-  igr <- asIgraph(agency_net)
+  if(type=="governance"){
+    net <- readRDS(paste0("data/network_",gspids[m]))
+    igr <- asIgraph(net)
+  }
+  if(type=="topic"){
+    net <- readRDS(paste0("data/topic_network_",gspids[m]))$posadj
+    igr <- igraph::graph.adjacency(net, mode = "undirected",weighted=NULL,diag=F)
+    net <- asNetwork(igr)
+  }
+  
   set.seed(327856)
   lc <- cluster_louvain(igr)
   #TODO add num communities
-  network_properties[m,] <- c(gspids[m], network::network.size(agency_net), network::network.edgecount(agency_net),
-                          network::network.density(agency_net),
-                          sna::connectedness(agency_net),sna::centralization(agency_net,sna::degree),
-                          length(unique(lc$membership)))
+  network_properties[m,] <- c(gspids[m], network::network.size(net), network::network.edgecount(net),
+                              network::network.density(net),
+                              sna::connectedness(net),sna::centralization(net,sna::degree),
+                              length(unique(lc$membership)), sna::gtrans(net, mode = "graph",
+                                                                         use.adjacency=F))
 }
 
-saveRDS(network_properties, "data/network_properties")
+if(type=="governance"){
+  
+  saveRDS(network_properties, "data/network_properties")
+  
+}
+if(type=="topic"){
+  
+  saveRDS(network_properties, "data/topic_network_properties")
+  
+}
+
 
 #extremes of connection and centralization
 min_connect <- network_properties_df %>% 
@@ -39,4 +64,3 @@ min_comm <- network_properties_df %>%
   filter(num_communities== min(num_communities))
 max_comm <- network_properties_df %>% 
   filter(num_communities== max(num_communities))
-
