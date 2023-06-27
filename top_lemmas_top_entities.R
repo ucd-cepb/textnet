aad460fef67315a81fdbf4975f18c992efd9e56a
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2023-06-27 14:40:04 -0700

    formatted as package function top_features

--- top_lemmas_top_entities.R
+++ top_lemmas_top_entities.R
@@ -1,63 +1,54 @@
-library(igraph)
-library(ohenery)
-library(tidyverse)
-gsp_text_with_meta <- readRDS("data/prepped_for_sna")
-gspids <- unique(gsp_text_with_meta$gsp_id)
-rm(gsp_text_with_meta)
+#Exported functions
+#top_features
 
-top_lemmas <- vector("list", length = length(gspids))
-all_lemmas<- vector("list", length = length(gspids))
-top_entities <- vector("list", length = length(gspids))
-all_entities <- vector("list", length = length(gspids))
-num_top <- 10
+#' @param files vector of filepaths to igraph objects or list of igraph objects
+#' @param from_file boolean whether files represent filepaths (T) or igraph objects (F)
+#' @return list of all entities and lemmas in the corpus, along with their average normalized prevalence as a fraction of a plan
+#' @import igraph
+#' @import ohenery
+#' @import tidyverse
+#' @import intergraph
+#' @export
 
-for(i in 1:length(gspids)){
-  igr <- readRDS(paste0("data/full_directed_graph_",gspids[i]))
-  net <- asNetwork(igr)
-  #get top 10
-  top_entities[[i]] <- head(sort(igraph::degree(igr),decreasing = T), num_top)
-  all_entities[[i]] <- sort(igraph::degree(igr),decreasing = T)
-  top_lemmas[[i]] <- head(sort(table(igraph::get.edge.attribute(
-    igr, "head_verb_lemma")), decreasing = T),num_top)
-  all_lemmas[[i]] <- sort(table(igraph::get.edge.attribute(
-    igr, "head_verb_lemma")), decreasing = T)
+top_features <- function(files, from_file=F){
+  all_lemmas<- vector("list", length = length(files))
+  all_entities <- vector("list", length = length(files))
+  
+  for(i in 1:length(files)){
+    
+    if(from_file==T){
+      igr <- readRDS(files[i])
+    }else{
+      igr <- files[[i]]
+    }
+
+    net <- asNetwork(igr)
+    all_entities[[i]] <- sort(igraph::degree(igr),decreasing = T)
+    all_lemmas[[i]] <- sort(table(igraph::get.edge.attribute(
+      igr, "head_verb_lemma")), decreasing = T)
+  }
+  
+  all_entities_normalized <- lapply(all_entities, function(x) ohenery::normalize(x))
+  all_lemmas_normalized <- lapply(all_lemmas, function(x) ohenery::normalize(x))
+  
+  all_entities_normalized <- unlist(all_entities_normalized)
+  all_lemmas_normalized <- unlist(all_lemmas_normalized)
+  
+  all_entities_df <- tibble("names" = names(all_entities_normalized), 
+                            "fraction_of_doc"= unname(all_entities_normalized))
+  all_lemmas_df <- tibble("names" =names(all_lemmas_normalized),
+                          "fraction_of_doc"=unname(all_lemmas_normalized))
+  #prevalence over entire corpus as avg fraction of a plan
+  all_entity_percents <- all_entities_df %>% group_by(names) %>% 
+    summarize("avg_fract_of_a_doc" = sum(fraction_of_doc)/length(files)) %>% arrange(desc(avg_fract_of_a_doc))
+  all_lemma_percents <- all_lemmas_df %>% group_by(names) %>% 
+    summarize("avg_fract_of_a_doc" = sum(fraction_of_doc)/length(files)) %>% arrange(desc(avg_fract_of_a_doc))
+  
+  #only keep entities that have letters
+  all_entity_percents <- all_entity_percents[grepl("[A-Za-z]", all_entity_percents$names),]
+  
+  return(list(entities = all_entity_percents, lemmas = all_lemma_percents))
+  
 }
 
-top_entities_normalized <- lapply(top_entities, function(x) ohenery::normalize(x))
-all_entities_normalized <- lapply(all_entities, function(x) ohenery::normalize(x))
-top_lemmas_normalized <- lapply(top_lemmas, function(x) ohenery::normalize(x))
-all_lemmas_normalized <- lapply(all_lemmas, function(x) ohenery::normalize(x))
 
-top_entities_normalized <- unlist(top_entities_normalized)
-all_entities_normalized <- unlist(all_entities_normalized)
-top_lemmas_normalized <- unlist(top_lemmas_normalized)
-all_lemmas_normalized <- unlist(all_lemmas_normalized)
-
-top_entities_df <- tibble("names" = names(top_entities_normalized), 
-                          "percent"= unname(top_entities_normalized))
-all_entities_df <- tibble("names" = names(all_entities_normalized), 
-                          "percent"= unname(all_entities_normalized))
-top_lemmas_df <- tibble("names" =names(top_lemmas_normalized),
-                        "percent"=unname(top_lemmas_normalized))
-all_lemmas_df <- tibble("names" =names(all_lemmas_normalized),
-                        "percent"=unname(all_lemmas_normalized))
-
-top_entity_percents <- top_entities_df %>% group_by(names) %>% 
-  summarize("total_percent" = sum(percent)) %>% arrange(desc(total_percent))
-all_entity_percents <- all_entities_df %>% group_by(names) %>% 
-  summarize("total_percent" = sum(percent)) %>% arrange(desc(total_percent))
-top_lemma_percents <- top_lemmas_df %>% group_by(names) %>% 
-  summarize("total_percent" = sum(percent)) %>% arrange(desc(total_percent))
-all_lemma_percents <- all_lemmas_df %>% group_by(names) %>% 
-  summarize("total_percent" = sum(percent)) %>% arrange(desc(total_percent))
-
-#only keep entities that have letters
-top_entity_percents <- top_entity_percents[grepl("[A-Za-z]", top_entity_percents$names),]
-all_entity_percents <- all_entity_percents[grepl("[A-Za-z]", all_entity_percents$names),]
-top_entity_percents
-all_entity_percents
-top_lemma_percents
-all_lemma_percents 
-
-saveRDS(all_entity_percents, "data/entity_prevalence_in_corpus_normalized")
-saveRDS(all_lemma_percents, "data/lemma_prevalence_in_corpus_normalized")
