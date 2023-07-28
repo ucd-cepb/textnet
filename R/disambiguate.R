0ec2efbe2e3ed98e9a3b5f1f6d31ce09684eba32
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2023-07-28 15:14:07 -0700

    List disambiguation

--- R/disambiguate.R
+++ R/disambiguate.R
@@ -30,6 +30,7 @@ disambiguate <- function(from, to, match_partial_entity=rep(F, length(from)), te
     warning("Removing ",sum(to_from_same)," rows in which to and from are identical.")
     from <- from[!(to_from_same)]
     to <- to[!(to_from_same)]
+    match_partial_entity <- match_partial_entity[!(to_from_same)]
     to_from_same <- sapply(1:length(to), function (j) sum(from[[j]]!=to[[j]])==0)
   }
   
@@ -72,6 +73,7 @@ disambiguate <- function(from, to, match_partial_entity=rep(F, length(from)), te
       if(length(to[[ndx]])>1){
         from <- from[-ndx]
         to <- to[-ndx]
+        match_partial_entity <- match_partial_entity[-ndx]
         removedelements <- append(removedelements,start)
       }else{
         #remove the end of the chain  
@@ -90,6 +92,7 @@ disambiguate <- function(from, to, match_partial_entity=rep(F, length(from)), te
         if(nxtfrom==start){
           from <- from[-currentndx]
           to <- to[-currentndx]
+          match_partial_entity <- match_partial_entity[-currentndx]
           removedelements <- append(removedelements, nxtfrom)
         }
       }
@@ -99,7 +102,24 @@ disambiguate <- function(from, to, match_partial_entity=rep(F, length(from)), te
   if(is_inf_loop==T){
     warning(paste0("to/from terms ", paste0(carryovers, collapse = ", "), " were in an infinite loop. ",
                    "Resolved by removing 'from' elements ",paste0(removedelements, collapse=", ")))
-  
+    #rerun the bit of code that finds the number of times to repeat now that the loop is resolved  
+    vectto <- unlist(to)
+    vectfrom <- unlist(from)
+    step0 <- which(vectto %in% vectfrom)
+    to0 <- vectto[step0]
+    carryovers <- to0[which(to0 %in% vectfrom)]
+    times_to_repeat <- 1
+    is_inf_loop <- F
+    to_nmin1 <- to0
+    while(length(carryovers)>0 & !is_inf_loop){
+      from_n <- vectfrom[which(vectfrom %in% carryovers)]
+      to_n <- unlist(to[
+        sapply(from, function(s) sum(s %in% from_n)>0)])
+      carryovers <- to_n[which(to_n %in% vectfrom)]
+      is_inf_loop <- !(length(to_n) <length(to_nmin1)) & length(carryovers)>0
+      to_nmin1 <- to_n
+      times_to_repeat <- times_to_repeat + 1
+    }
   }
   
   #Section 1: Drop "The"####
@@ -129,53 +149,86 @@ disambiguate <- function(from, to, match_partial_entity=rep(F, length(from)), te
     multi_from <- sapply(1:length(from), function(w) length(from[[w]]) > 1)
     
     if(sum(multi_from)>0){
-      stop("Elements in 'from' may not be lists. Please change elements ", paste0(which(multi_to), collapse = ", ") ," to a single character vector.")
+      stop("Elements in 'from' may not be lists. Please change elements ", 
+           paste0(which(multi_to), collapse = ", ") ," to a single character vector.")
+    }
+    if(sum(multi_to==T & match_partial_entity==T)>0){
+      stop("Elements for which 'to' is a list may not be matched on a partial string. Please set match_partial_entity to F for these elements.")
     }
     
     #if the extract matches something that's supposed to turn into a list, send it to a temp column
     
-    multi_match_partials <- match_partial_entity[multi_to]
-    froms_of_multi_to <- from[multi_to]
     
-    #TODO only if there is only one entity in the cell.
-    partial_multi_froms <- froms_of_multi_to[multi_match_partials]
-    partial_multi_tos <- multi_to[multi_match_partials]
-    full_multi_froms <- froms_of_multi_to[!multi_match_partials]
-    full_multi_tos <- multi_to[!multi_match_partials]
-    
-    if(length(partial_multi_froms)>0){
-      #beginning of word
-      begf <- paste0("^",partial_multi_froms,"_")
-      begt <- paste0(partial_multi_tos,"_")
-      #middle or end of word
-      midf <- paste0("_",partial_multi_froms)
-      midt <- paste0("_",partial_multi_tos)
-      #entire word
-      wholef <- paste0("^",partial_multi_froms,"$")
-      wholet <- partial_multi_tos
+    sub_try_drop_forlists <- function(remove, terms, didntmatch){
+      tempv <- terms
+      rem <- grepl(paste(remove,collapse = '|'),terms,perl = T)
+      tempv[rem ==T & didntmatch==T] <- str_remove_all(tempv[rem==T& didntmatch==T],paste(remove,collapse = '|'))
       
-      frommultipartial <- c(begf, midf, wholef)
-      tomultipartial <- c(begt,midt,wholet)
+      #step two, now that try_drop is removed, does it match the from? if so, substitute the from.
+      #then return only the terms that actually changed
+      
+      infrom <- which(grepl(paste(fromregex,collapse='|'),tempv,perl=T))
+      terms[infrom] <- tempv[infrom]
+      
+      
+      return(terms)
     }
-    #entire word
-    frommultifull <- paste0("^",full_multi_froms,"$")
-    tomultifull <- full_multi_tos
     
-    fromregex <- c(frommultipartial, frommultifull)
-    toregex <- c(tomultipartial, tomultifull)
+    froms_of_multi_to <- from[multi_to]
+    tos_of_multi_to <- to[multi_to]
     
-    namedvect <- toregex
-    names(namedvect) <- fromregex
+    textnet_extract$edgelist$sourcetemp <- textnet_extract$edgelist$source
+    textnet_extract$edgelist$targettemp <- textnet_extract$edgelist$target
+    textnet_extract$nodelist$entity_cattemp <- textnet_extract$nodelist$entity_cat
     
+    index <- rep(F, length=length(textnet_extract$edgelist$sourcetemp))
+    for(q in 1:length(froms_of_multi_to)){
+      index <- ifelse(is.na(textnet_extract$edgelist$sourcetemp),
+                  F, ifelse(textnet_extract$edgelist$sourcetemp == froms_of_multi_to[[q]], 
+                            T, index))
+      
+      textnet_extract$edgelist$sourcetemp <- ifelse(is.na(textnet_extract$edgelist$sourcetemp),
+                 NA, ifelse(textnet_extract$edgelist$sourcetemp == froms_of_multi_to[[q]], 
+                            tos_of_multi_to[q], textnet_extract$edgelist$sourcetemp))
+        
+    }
+    notindex <- !index
+    if(!is.null(try_drop)){
+      textnet_extract$edgelist$sourcetemp <- sub_try_drop_forlists(try_drop, textnet_extract$edgelist$sourcetemp, notindex)
+    }
     
-    index <- which(grepl(paste(fromregex,collapse='|'),textnet_extract$edgelist$source,perl=T))
-    notindex <- which(!grepl(paste(fromregex,collapse='|'),textnet_extract$edgelist$source,perl=T))
-    textnet_extract$edgelist$source[index] <- str_replace_all(textnet_extract$edgelist$source[index],
-                                                              namedvect)
+    index <- rep(F, length=length(textnet_extract$edgelist$targettemp))
+    for(q in 1:length(froms_of_multi_to)){
+      index <- ifelse(is.na(textnet_extract$edgelist$targettemp),
+                      F, ifelse(textnet_extract$edgelist$targettemp == froms_of_multi_to[[q]], 
+                                T, index))
+      
+      textnet_extract$edgelist$targettemp <- ifelse(is.na(textnet_extract$edgelist$targettemp),
+                                                    NA, ifelse(textnet_extract$edgelist$targettemp == froms_of_multi_to[[q]], 
+                                                               tos_of_multi_to[q], textnet_extract$edgelist$targettemp))
+      
+    }
+    notindex <- !index
+    if(!is.null(try_drop)){
+      textnet_extract$edgelist$targettemp <- sub_try_drop_forlists(try_drop, textnet_extract$edgelist$targettemp, notindex)
+    }
+
+    index <- rep(F, length=length(textnet_extract$nodelist$entity_cattemp))
+    for(q in 1:length(froms_of_multi_to)){
+      index <- ifelse(is.na(textnet_extract$nodelist$entity_cattemp),
+                      F, ifelse(textnet_extract$nodelist$entity_cattemp == froms_of_multi_to[[q]], 
+                                T, index))
+      
+      textnet_extract$nodelist$entity_cattemp <- ifelse(is.na(textnet_extract$nodelist$entity_cattemp),
+                                                    NA, ifelse(textnet_extract$nodelist$entity_cattemp == froms_of_multi_to[[q]], 
+                                                               tos_of_multi_to[q], textnet_extract$nodelist$entity_cattemp))
+      
+    }
+    notindex <- !index
+    if(!is.null(try_drop)){
+      textnet_extract$nodelist$entity_cattemp <- sub_try_drop_forlists(try_drop, textnet_extract$nodelist$entity_cattemp, notindex)
+    }
     
-    #temp rows because changing character column into list column
-    edgelist$sourcetemp <- lapply(edgelist$source, function(strng) agency_disambig(strng,m))
-    edgelist$targettemp <- lapply(edgelist$target, function(strng) agency_disambig(strng,m))
     edgelist$length_source <- sapply(edgelist$sourcetemp, length)
     edgelist$length_target <- sapply(edgelist$targettemp, length)
     rows <- edgelist[rep(seq(1, nrow(edgelist)), edgelist$length_source)]
@@ -193,7 +246,7 @@ disambiguate <- function(from, to, match_partial_entity=rep(F, length(from)), te
     rows$entity_cattemp <- rows$length_entitycat <- NULL
     nodelist <- rows
     
-    #Subsection 2: Match Partial and Full Words, and TryDrop####
+    #Subsection 2: Match Partial and Full Words (Non-List), and TryDrop####
     #if doesn't match, remove try_drop from the edgelist and nodelist and see if it matches
     sub_try_drop <- function(remove, terms, didntmatch){
       tempv <- terms
@@ -209,18 +262,18 @@ disambiguate <- function(from, to, match_partial_entity=rep(F, length(from)), te
       
       #if removing the try_drop causes the entity to match the entire entry of a "to" column
       #make it that entity
-      towhole <- paste0("^",to,"$")
+      towhole <- paste0("^",to[!multi_to],"$")
       into <- which(grepl(paste(towhole, collapse='|'),tempv,perl=T))
       terms[into] <- tempv[into]
       
       return(terms)
     }
     
-    #TODO only if there is only one entity in the cell.
+    #only if there is only one entity in the cell.
     frompartial <- from[match_partial_entity]
     topartial <- to[match_partial_entity]
-    fromfull <- from[!match_partial_entity]
-    tofull <- to[!match_partial_entity]
+    fromfull <- from[!match_partial_entity & !multi_to]
+    tofull <- to[!match_partial_entity & !multi_to]
     
     if(length(frompartial)>0){
       #beginning of word
